d6edca3d81b370626fa0989a651026c97687609a
Spoorti <spoortisatenahalli@gmail.com>
2022-05-23 18:41:15 +0530

    first

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ContactUs = () => {
+  let name="SPOORTI"
+  return (
+    <>
+    <h1>Contact Us</h1>
+    <div className='cont' >
+      <br />
+      <br />
+      <div>
+      📞   Phone Number: <a href="tel:+91-8299098719">+91 6360688133</a>
+      <br />
+      <br />
+          📧 E-Mail: <a href="mailto : spoortisatenahalli">spoortisatenahalli@gmail.com</a>
+      <br /><br />
+
+  
+      
+      </div>
+     
+      
+
+    </div>
+    <br /> <h3 >{name}</h3>
+    </>
+  )
+}
+
+export default ContactUs
