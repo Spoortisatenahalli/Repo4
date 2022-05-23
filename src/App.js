88e1f3578e53a53d99d9288d3c6639d4547c35f2
Spoorti <71556843+Spoortisatenahalli@users.noreply.github.com>
2022-05-23 19:19:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React, { }  from 'react';
 import './App.css';
 import Students from './components/Students'
 import ContactUs from './components/ContactUs'
